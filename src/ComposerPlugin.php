0a91903cc1c56c18ce93acc0c3960f6cdb73ae87
Hart Joris <joris@ati4group.com>
2022-03-19 21:48:53 +0100

    Add the composer plugin and extract information of the registration.php

--- src/ComposerPlugin.php
+++ src/ComposerPlugin.php
@@ -13,9 +13,14 @@ use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 use Composer\Script\Event;
 use Composer\Script\ScriptEvents;
+use Magento\Framework\Component\ComponentRegistrar;
 
 final class ComposerPlugin implements PluginInterface, EventSubscriberInterface
 {
+    const GLOB_LIST_PATH = '/app/etc/registration_globlist.php';
+
+    private $projectRoot = null;
+
     public static function getSubscribedEvents(): array
     {
         return [
@@ -26,7 +31,6 @@ final class ComposerPlugin implements PluginInterface, EventSubscriberInterface
 
     public function activate(Composer $composer, IOInterface $io): void
     {
-
     }
 
     public function compileRegistration(Event $event): void
@@ -34,11 +38,63 @@ final class ComposerPlugin implements PluginInterface, EventSubscriberInterface
         $io = $event->getIO();
         $composer = $event->getComposer();
 
-        $io->write('<info>Dump components registration file:</info>');
+        $this->projectRoot = dirname($composer->getConfig()->get('vendor-dir'));
 
-        $packages = $composer->getRepositoryManager()->getLocalRepository()->getPackages();
+        $modules = $this->getNonComposerRegistrationList($io);
 
+        foreach ($modules as $module) {
+            $io->write(implode(',', $module));
+        }
+    }
 
-        // ToDo: To Implement
+    private function getNonComposerRegistrationList(IOInterface $io): array
+    {
+        $globPatterns = require $this->projectRoot . self::GLOB_LIST_PATH;
+        $modules = [];
+
+        foreach ($globPatterns as $globPattern) {
+            $files = glob($this->projectRoot . DIRECTORY_SEPARATOR . $globPattern, GLOB_NOSORT);
+
+            if ($files === false) {
+                $io->writeError("glob(): error with '$this->projectRoot$globPattern'");
+            }
+
+            foreach ($files as $file) {
+                $modules[] = $this->parseRegistrationFile($file, $io);
+            }
+        }
+
+        return $modules;
+    }
+
+    private function parseRegistrationFile(string $filePath, $io): ?array
+    {
+        $content = file_get_contents($filePath);
+        $pattern = '{::register\([ \r\n\t]*(?<type>[\'\\\w]+?(::)\w+)[ \r\n\t]*,[ \r\n\t]*["\'](?<name>[\/\w]+)["\']}';
+
+        $io->write($pattern);
+        $io->write($content);
+        $io->write(preg_match($pattern, $content, $matches));
+
+        if (preg_match($pattern, $content, $matches)) {
+
+            $io->write($matches);
+
+            include $filePath;
+
+            $register = new ComponentRegistrar();
+
+            return [
+                'type' => $matches['type'],
+                'componentName' => $matches['name'],
+                'path' =>  $register->getPath($matches['type'], $matches['name'])
+            ];
+        }
+
+        return null;
+    }
+
+    private function generateGlobalRegistrationFile(): void
+    {
     }
 }
