d7705d3f3429d5d35088be268f4f1fe1f7615a11
Thomas Klein <thomas.klein@mpbio.com>
2022-03-20 15:34:46 +0100

    optimize composer plugin, dump all registrar at once

--- src/ComposerPlugin.php
+++ src/ComposerPlugin.php
@@ -15,7 +15,12 @@ use Composer\Script\Event;
 use Composer\Script\ScriptEvents;
 use Generator;
 use Laminas\Code\DeclareStatement;
+use Laminas\Code\Generator\BodyGenerator;
 use Laminas\Code\Generator\FileGenerator;
+use Laminas\Code\Generator\GeneratorInterface;
+use Laminas\Code\Generator\ValueGenerator;
+use Magento\Framework\Component\ComponentRegistrar;
+use ReflectionClass;
 use function fclose;
 use function fopen;
 use function fwrite;
@@ -24,6 +29,7 @@ use function glob;
 use function sprintf;
 use const DIRECTORY_SEPARATOR;
 use const GLOB_NOSORT;
+use const PHP_EOL;
 
 final class ComposerPlugin implements PluginInterface, EventSubscriberInterface
 {
@@ -57,20 +63,52 @@ final class ComposerPlugin implements PluginInterface, EventSubscriberInterface
         $io->write('<info>Dump components registration file...</info>');
 
         $basePath = getcwd();
-        $filePath = $basePath . '/app/etc/NonComposerComponentRegistration.php';
+        $fileName = 'app/etc/NonComposerComponentRegistration.php';
+
+        $this->dumpRegistration($basePath, $fileName);
+
+        $io->write('<info>Dumped at <comment>`' . $basePath . DIRECTORY_SEPARATOR . $fileName . '`</comment>!</info>');
+    }
+
+    private function dumpRegistration(string $basePath, string $fileName): void
+    {
+        $filePath = $basePath . DIRECTORY_SEPARATOR . $fileName;
         $registrar = fopen($filePath, 'w+b');
 
         $fileGenerator = new FileGenerator();
         $fileGenerator->setDeclares([DeclareStatement::strictTypes(1)]);
+        $fileGenerator->setUse(ComponentRegistrar::class);
         fwrite($registrar, $fileGenerator->generate());
 
+        foreach ($this->resolveComponents($basePath) as $type => $components) {
+            foreach ($components as $name => $path) {
+                fwrite($registrar, $this->registrarGenerator($type, $name, $path)->generate() . PHP_EOL);
+            }
+        }
+        fclose($registrar);
+    }
+
+    private function registrarGenerator(string $type, string $name, string $path): GeneratorInterface
+    {
+        return (new BodyGenerator())->setContent(sprintf(
+            'ComponentRegistrar::register(%s, %s, %s);',
+            (new ValueGenerator($type))->generate(),
+            (new ValueGenerator($name))->generate(),
+            (new ValueGenerator($path))->generate()
+        ));
+    }
+
+    private function resolveComponents(string $basePath): array
+    {
         foreach ($this->globRegistrations($basePath) as $registration) {
-            fwrite($registrar, sprintf("include '%s';\n", $basePath . DIRECTORY_SEPARATOR . $registration));
+            include $registration;
         }
 
-        fclose($registrar);
+        $reflection = new ReflectionClass(ComponentRegistrar::class);
+        $pathsProperty = $reflection->getProperty('paths');
+        $pathsProperty->setAccessible(true);
 
-        $io->write('<info>Dumped at <comment>`' . $filePath . '`</comment>!</info>');
+        return $pathsProperty->getValue();
     }
 
     private function globRegistrations(string $basePath): Generator
